0a460b5203d8eb9c154701925e909e2f67d21131
abirbot1212 <sami147islam@gmail.com>
2025-05-05 12:54:53 +0600

    Update info.js

--- Script/commands/info.js
+++ Script/commands/info.js
@@ -23,26 +23,24 @@ const time = process.uptime(),
 		seconds = Math.floor(time % 60);
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
-var link = ["https://i.imgur.com/afSpOv6.gif", "https://i.imgur.com/afSpOv6.gif", "https://i.imgur.com/afSpOv6.gif", "https://i.imgur.com/afSpOv6.gif", "https://i.imgur.com/fHgoeUS.gif", "https://i.imgur.com/xLS68nV.gif", "https://i.imgur.com/afSpOv6.gif", "https://i.imgur.com/afSpOv6.gif"];
+var link = ["https://i.imgur.com/afSpOv6.gif", "https://i.postimg.cc/rpLppqWX/IMG-20250503-182404.jpg", "https://i.postimg.cc/rpLppqWX/IMG-20250503-182404.jpg", "https://i.postimg.cc/rpLppqWX/IMG-20250503-182404.jpg", "https://i.postimg.cc/rpLppqWX/IMG-20250503-182404.jpg", "https://i.postimg.cc/rpLppqWX/IMG-20250503-182404.jpg", "https://i.postimg.cc/rpLppqWX/IMG-20250503-182404.jpg", "https://i.postimg.cc/rpLppqWX/IMG-20250503-182404.jpg"];
 var callback = () => api.sendMessage({body:`✦𝗔𝗗𝗠𝗜𝗠 𝗔𝗡𝗗 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗜𝗢𝗡✦
 
 ⁂BoT NaMe ⊂◉‿◉: ${global.config.BOTNAME}
 
 ✡BoT Prefix ◉‿◉: ${global.config.PREFIX}
 
-༻𝐎𝐖𝐍𝐄𝐑:- ☞𝐑𝐚𝐭𝐮𝐥 𝐇𝐚𝐬𝐬𝐚𝐧☜ ༺
-༒𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 〠𝐌𝐫𝐓𝐨𝐦𝐗𝐱𝐗〠.༒
+༻𝐎𝐖𝐍𝐄𝐑:- ☞ABIR HASAN☜ ༺
+༒𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 〠ABIR〠.༒
 
-༒𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝༒:- ☞ www.facebook.com/MrTomXxX ☜ 
+༒𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝༒:- ☞ https://www.facebook.com/ABIRMAHMMUD1344 ☜ 
 
-༻𝘠𝘰𝘶𝘵𝘶𝘣𝘦 𝘭𝘪𝘯𝘬༺:- 
-☞ https://youtube.com/@MrT0mX ☜
+༻WEBSITE 𝘭𝘪𝘯𝘬༺:- 
+☞ https://abir7109.github.io/cyberabir/ ☜
 
-֎𝘍𝘰𝘳 𝘈𝘯𝘺 𝘒𝘪𝘯𝘥 𝘖𝘧 𝘏𝘦𝘭𝘱 𝘫𝘰𝘪𝘯 𝘞𝘱 𝘎𝘳𝘰𝘶𝘱֍:-
+֎𝘍𝘰𝘳 𝘈𝘯𝘺 𝘒𝘪𝘯𝘥 𝘖𝘧 𝘏𝘦𝘭𝘱 ֍:-
 
-☞ https://chat.whatsapp.com/Hog9Xpbgz9cK7EpfUKaHnC ☜
-
- ֎𝘊𝘰𝘯𝘵𝘢𝘤𝘵 𝘔𝘦 𝘖𝘯 𝘞𝘩𝘢𝘵𝘴𝘈𝘱𝘱֍ :-  ☞+8801581712206 ☜
+ ֎𝘊𝘰𝘯𝘵𝘢𝘤𝘵 𝘔𝘦 𝘖𝘯 𝘞𝘩𝘢𝘵𝘴𝘈𝘱𝘱֍ :-  ☞+8801919069898 ☜
  
 ➟UPTIME☆
 
