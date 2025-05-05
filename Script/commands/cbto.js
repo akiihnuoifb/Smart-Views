42daf160dfe37270e4ef9ee13e20aae5937b1d07
abirbot1212 <sami147islam@gmail.com>
2025-05-05 10:59:34 +0600

    Update cbto.js

--- Script/commands/cbto.js
+++ Script/commands/cbto.js
@@ -17,22 +17,22 @@ module.exports.config = {
 module.exports.handleEvent = async ({ api, event, Threads,Users}) => {
 var id = event.senderID;
  var name = await Users.getNameUser(event.senderID);
- if (event.body.indexOf("ULLASH")==0 || (event.body.indexOf("ullash bau")==0) || event.body.indexOf("উল্লাস ভাই")==0 ||
-event.body.indexOf("Ullash bai")==0 ||
-event.body.indexOf("ullash dadu")==0 ||
-event.body.indexOf("উল্লাস")==0 ||
-event.body.indexOf("ullash")==0 ||
-event.body.indexOf("ulash")==0 ||
-event.body.indexOf("ullas")==0 ||
-event.body.indexOf("ullah")==0) {
+ if (event.body.indexOf("ABIR")==0 || (event.body.indexOf("abir vai")==0) || event.body.indexOf("আবির ভাই")==0 ||
+event.body.indexOf("Abir vai")==0 ||
+event.body.indexOf("Abir vai")==0 ||
+event.body.indexOf("আবির")==0 ||
+event.body.indexOf("Abir")==0 ||
+event.body.indexOf("abir")==0 ||
+event.body.indexOf("abir")==0 ||
+event.body.indexOf("Abir")==0) {
  const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
  var link = [
-"https://i.imgur.com/fGY8plC.mp4",
-"https://i.imgur.com/fGY8plC.mp4",
+"",
+"",
  ];
- var callback = () => api.sendMessage({body:`╭•┄┅════❁🌺❁════┅┄•╮\n প্রিয় মানুষ ডুবে থাকুক,,,\nহৃদওপিন্ডের বিশাল গভিরতায়,,!🌸 \n╰•┄┅════❁🌺❁════┅┄•╯\n\n ${name} উল্লাস এর ইনবক্স :- m.me/100086680386976 \n⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆`,attachment: fs.createReadStream(__dirname + "/cache/emon.jpeg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/emon.jpeg"), event.messageID);
+ var callback = () => api.sendMessage({body:`╭•┄┅════❁🌺❁════┅┄•╮\n প্রিয় মানুষ ডুবে থাকুক,,,\nহৃদওপিন্ডের বিশাল গভিরতায়,,!🌸 \n╰•┄┅════❁🌺❁════┅┄•╯\n\n ${name} আবির এর ইনবক্স :- m.me/61556527816380 \n⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆`,attachment: fs.createReadStream(__dirname + "/cache/")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/"), event.messageID);
  const timeStart = Date.now();
  const dcm = process.uptime(); 
  var anh = Math.floor(dcm / (60 * 60));
